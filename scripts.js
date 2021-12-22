c005e0b596440da68c79f5ba2c8abf6314ed1fd6
3millien <emillien.pearce@gmail.com>
2021-12-22 21:04:47 +0200

    Started a function..

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 let my_Library = [];
 
-function Book() {
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return title, author, pages, read;
+        
+    }
     //the constructor
 }
+
 
 function add_Book_To_Library() {
     //do stuff here
